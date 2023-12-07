bbef1e949ba21a69a338639ca4af5c30d9d35945
Piootrekk <piotr.wilkowiecki100@gmail.com>
2023-12-07 17:33:51 +0100

    FireBase database add items method

--- src/layout/Details/DetailsReview.vue
+++ src/layout/Details/DetailsReview.vue
@@ -2,11 +2,13 @@
   <h1 class="text-3xl text-center mb-8">Your reviews</h1>
   <p class="ml-5 text-xl">Add your review</p>
   <div class="flex ml-5">
-    <CustomInput class="h-14 w-full mb-0 mt-0 mr-4" />
+    <CustomInput class="h-14 w-full mb-0 mt-0 mr-4" v-model="inputRef" />
     <CustomButton
       class="h-14 w-1/5 mb-12 mt-8"
       label="Add"
       :handleClick="AddData"
+      :isButtonDisabled="inputRef.length === 0"
+      :isLoading="isLoading"
     ></CustomButton>
   </div>
   <h1 class="text-3xl text-center mb-8">Other reviews</h1>
@@ -38,7 +40,7 @@ import CustomButton from "@/components/CustomButton.vue";
 import CustomInput from "@/components/CustomInput.vue";
 import ReviewsSection from "./ReviewsSection.vue";
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 export default {
   name: "DetailsReview",
   components: {
@@ -52,16 +54,36 @@ export default {
       required: true,
     },
   },
-  setup() {
+  setup(props) {
+    const inputRef = ref("");
+    const isLoading = ref(false);
     const store = useStore();
     const user = computed(() => store.getters["firebaseAuthModule/getUser"]);
-    const reviews = computed(() => store.getters["firebaseDatabaseModule/getReviews"]);
+    // const reviews = computed(
+    //   () => store.getters["firebaseDatabaseModule/getReviews"]
+    // );
 
-    const AddData = () => {
-      console.log("user: ",user);
-      console.log(reviews);
+    const AddData = async () => {
+      isLoading.value = true;
+      let docTOSend = {
+        movieId: props.movie.imdbID,
+        id_user: user.value.uid,
+        mail: user.value.email,
+        rating: "",
+        comment: inputRef.value,
+      };
+      console.log(docTOSend);
+      await store.dispatch("firebaseDatabaseModule/addReview", {
+        data: docTOSend,
+        collectionName: "Reviews",
+      });
+
+      isLoading.value = false;
     };
-    return { AddData };
+    return { AddData, inputRef, isLoading };
   },
 };
 </script>
+
+// { id: "", movieId: "", user: { id: "", mail: "", }, rating: "", comment: "",
+// createdAt: "", },
--- src/store/modules/firebaseDatabaseModule.js
+++ src/store/modules/firebaseDatabaseModule.js
@@ -1,27 +1,16 @@
 // firebaseDatabaseModule.js
 import { db } from "@/firebase/config.js";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 export default {
   namespaced: true,
   state: {
-    Reviews: [
-      {
-        id: "",
-        movieId: "",
-        user: {
-          id: "",
-          mail: "",
-        },
-        rating: "",
-        comment: "",
-        createdAt: "",
-      },
-    ],
+    Reviews: [],
     isLoading: false,
   },
   mutations: {
     setReviews(state, payload) {
-      state.Reviews = payload;
+      state.Reviews.push(payload);
     },
     setIsLoading(state, payload) {
       state.isLoading = payload;
@@ -36,33 +25,39 @@ export default {
     },
   },
   actions: {
-    async fetchReviews({ commit }, movieId) {
-      commit("setIsLoading", true);
-      const reviews = [];
-      const querySnapshot = await db
-        .collection("Reviews")
-        .where("movieId", "==", movieId)
-        .get();
-      querySnapshot.forEach((doc) => {
-        reviews.push(doc.data());
-      });
-      commit("setReviews", reviews);
-      commit("setIsLoading", false);
+    async addReview({ commit }, obj) {
+      try {
+        commit("setIsLoading", true);
+        console.log("collectionName:", obj.collectionName);
+        const reviewsCollectionRef = collection(
+          db,
+          obj.collectionName.toString()
+        );
+        obj.data.createdAt = serverTimestamp();
+        await addDoc(reviewsCollectionRef, obj.data);
+        commit("setReviews", obj.data);
+        console.log("Review added");
+      } catch (error) {
+        console.error("Error adding review:", error);
+      } finally {
+        commit("setIsLoading", false);
+      }
     },
-    async addReview({ commit }, payload) {
-      commit("setIsLoading", true);
-      await db.collection("Reviews").add(payload);
-      commit("setIsLoading", false);
-    },
-    async updateReview({ commit }, { reviewId, updatedData }) {
-      commit("setIsLoading", true);
-      await db.collection("Reviews").doc(reviewId).update(updatedData);
-      commit("setIsLoading", false);
-    },
-    async deleteReview({ commit }, reviewId) {
-      commit("setIsLoading", true);
-      await db.collection("Reviews").doc(reviewId).delete();
-      commit("setIsLoading", false);
+    async getReviews({ commit }, collectionName) {
+      try {
+        commit("setIsLoading", true);
+        const reviewsCollectionRef = collection(db, collectionName);
+        const reviewsSnapshot = await reviewsCollectionRef.get();
+        reviewsSnapshot.forEach((doc) => {
+          commit("setReviews", doc.data());
+        });
+      } catch (error) {
+        console.error("Error getting reviews:", error);
+      } finally {
+        commit("setIsLoading", false);
+        console.log("Reviews fetched");
+        console.log("Reviews:", this.state.Reviews);
+      }
     },
   },
 };
